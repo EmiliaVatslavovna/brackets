3f4d8dee9bab1bf0ced31352f4212e40f349d97a
EmiliaVatslavovna <emilia.podgaiskaya@mail.ru>
2023-08-12 23:43:51 +0300

    fix: start use bracketsConfig

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  let result = '';
+    let result = 0;
+    for (let i = 0; i < str.lenght; i++) {
 
-  for (let i = 0; i < str.lenght; i++) {
-    if (str[i] = ')') {result += 1;}
-    if (str[i] = '(') {result -= 1;}
-    if (str[i] = ']') {result += 1;}
-    if (str[i] = '[') {result -= 1;}
-  }
-  if (result === 0) {return true;}
-  else return false; 
+        if (str[i] = bracketsConfig[0][0]) { result++; }
+        else if (str[i] = bracketsConfig[0][1]) { result--; }
+
+        if (str[i] = bracketsConfig[1][0]) { result++; }
+        else if (str[i] = bracketsConfig[1][1]) { result--; }
+
+        if (str[i] = bracketsConfig[2][0]) { result++; }
+        else if (str[i] = bracketsConfig[2][1]) { result--; }
+
+        if (str[i] = bracketsConfig[3][0]) { result++; }
+        else if (str[i] = bracketsConfig[1][1]) { result--; }
+
+        if (str[i] = bracketsConfig[4][0]) { result++; }
+        else if (str[i] = bracketsConfig[4][1]) { result--; }
+
+        if (str[i] = bracketsConfig[5][0]) { result++; }
+        else if (str[i] = bracketsConfig[5][1]) { result--; }
+    }
+    if (result === 0) { return true; }
+    else return false;
 }

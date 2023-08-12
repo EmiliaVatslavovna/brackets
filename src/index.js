421dd0383b2b41a9d033c2779abd6c11991063ef
EmiliaVatslavovna <emilia.podgaiskaya@mail.ru>
2023-08-12 18:09:04 +0300

    feat: check str

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = '';
+
+  for (let i = 0; i < str.lenght; i++) {
+    if (str[i] = ')') {result += 1;}
+    if (str[i] = '(') {result -= 1;}
+    if (str[i] = ']') {result += 1;}
+    if (str[i] = '[') {result -= 1;}
+  }
+  if (result === 0) {return true;}
+  else return false; 
 }
